9f48af4ed919548addb1961e3adbe450db4aa4fe
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-09 03:09:52 +0430

    Add routes and router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Books from "./components/Books";
-import Book from "./components/Books";
 import Categories from "./components/Categories";
 
 const App = () => {
@@ -19,10 +18,10 @@ const App = () => {
       </nav>
       <Switch>
         <Route path="/categories">
-            <Categories/>
+          <Categories />
         </Route>
         <Route path="/">
-            <Books/>
+          <Books />
         </Route>
       </Switch>
     </Router>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import App from "./app";
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
