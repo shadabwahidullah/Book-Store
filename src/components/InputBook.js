e8707673fb1b1fce1ec00f1c3c282132b1d21374
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-09 23:39:00 +0430

    Create UUID and AddNewBook

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import uuid from 'react-uuid';
+import { addBook } from '../redux/books/Books';
+
+const dispatch  = useDispatch();
+
+const addNewBook = () => {
+  const newBook = {
+    id: {uuid()},
+    title,
+    author,
+  }
+
+  dispatch(addBook(newBook));
+}
 
 const InputBook = () => (
-  <form>
+  <form onSubmit={addNewBook()}>
     <input placeholder="name of the book" />
     <input placeholder="author of the book" />
     <button type="submit">Add book</button>
