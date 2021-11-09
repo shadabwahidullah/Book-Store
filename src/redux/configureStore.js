8c0dc595741e45b1f2f550f131b9d3049dd6b7f5
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-09 23:19:10 +0430

    Configure and create store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,1 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+const reducer = combineReducers({
+
+});
+
+const store = createStore(
+    reducer,
+    applyMiddleware(logger)
+);
+
+export default store;
