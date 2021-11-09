2a64aef00870b7a9be2799e787ce1e1484ccbb18
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-09 23:29:15 +0430

    Add books reducer in configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import booksReducer from "./books/Books.js";
 
 const reducer = combineReducers({
-
+  //reducers come here
+  booksReducer,
 });
 
-const store = createStore(
-    reducer,
-    applyMiddleware(logger)
-);
+const store = createStore(reducer, applyMiddleware(logger));
 
 export default store;
