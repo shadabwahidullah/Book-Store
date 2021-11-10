e0856540af53a4919dd9b7540ecd52f4f66df8f2
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-10 22:03:31 +0430

    Add form validation

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import uuid from 'react-uuid';
+import { useForm } from 'react-hook-form';
 import { addBook } from '../redux/books/Books';
 
 const InputBook = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
   const dispatch = useDispatch();
 
   const addNewBook = (event) => {
     const id = uuid();
-    const title = event.target.children.bookTitle.value;
-    const author = event.target.children.bookAuthor.value;
+    const { title } = event;
+    const { author } = event;
+
     const newBook = {
       id,
       title,
       author,
     };
     dispatch(addBook(newBook));
+    reset();
   };
 
   return (
     <form
-      onSubmit={(event) => {
-        event.preventDefault();
+      onSubmit={handleSubmit((event) => {
+        console.log('printed from handle submit', event);
         addNewBook(event);
-      }}
+      })}
     >
-      <input id="bookTitle" placeholder="name of the book" />
-      <input id="bookAuthor" placeholder="author of the book" />
+      {/* eslint-disable react/jsx-props-no-spreading */}
+      <input
+        {...register('title', { required: true })}
+        placeholder="name of the book"
+      />
+      {errors.title?.type === 'required' && 'Title is required'}
+      <input
+        {...register('author', { required: true })}
+        placeholder="author of the book"
+      />
+      {errors.author?.type === 'required' && 'Author is required'}
       <button type="submit">Add book</button>
     </form>
   );
