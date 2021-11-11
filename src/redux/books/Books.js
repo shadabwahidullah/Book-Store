597ea57d6805dbea1fc0e68ea2638c0c4f020d46
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-12 01:23:32 +0430

    Fetch data from API successfully

--- src/redux/books/Books.js
+++ src/redux/books/Books.js
@@ -1,6 +1,13 @@
+import axios from 'axios';
+
 // action types
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_BOOKS = 'bookStore/books/FETCH_BOOKS';
+
+// API
+const APP_ID = 's9WgIk6JBNM28R0fePw0';
+const BASE_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APP_ID}/books`;
 
 // initial state
 const initialState = [];
@@ -15,6 +22,27 @@ export const removeBook = (payload) => ({
   type: REMOVE_BOOK,
   payload,
 });
+export const fetchBooks = (payload) => ({
+  type: FETCH_BOOKS,
+  payload,
+});
+
+export const fetchBooksAction = () => {
+  axios.get(BASE_URL).then((res) => {
+    const books = Object.entries(res.data);
+    const fetchedBooks = [];
+    console.log(books.length);
+    books.forEach((book) => {
+      const tmpBook = {
+        id: book[0],
+        title: book[1][0].title,
+        category: book[1][0].category,
+      };
+      fetchedBooks.push(tmpBook);
+    });
+    return fetchedBooks;
+  });
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
@@ -22,6 +50,8 @@ const reducer = (state = initialState, action) => {
       return [...state, action.payload];
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
+    case FETCH_BOOKS:
+      return fetchBooksAction();
     default:
       return state;
   }
