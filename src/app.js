7a6159cf648388d9dcebbd4e58c4f2c02123fd65
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-12 22:38:42 +0430

    Style the panel

--- src/app.js
+++ src/app.js
@@ -6,26 +6,28 @@ import BooksPage from './components/BooksPage';
 import Categories from './components/Categories';
 
 const App = () => (
-  <Router>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Books</Link>
-        </li>
-        <li>
-          <Link to="/categories">Categories</Link>
-        </li>
-      </ul>
-    </nav>
-    <Switch>
-      <Route exact path="/categories">
-        <Categories />
-      </Route>
-      <Route exact path="/">
-        <BooksPage />
-      </Route>
-    </Switch>
-  </Router>
+  <div className="panel-bg">
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Books</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/categories">
+          <Categories />
+        </Route>
+        <Route exact path="/">
+          <BooksPage />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
 );
 
 export default App;
