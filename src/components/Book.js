1bc793f125b92ce904728cfe653a6fb28cf47084
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-13 00:15:41 +0430

    Add style to the left part of the book panel

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { removeBookAPI } from '../redux/books/Books';
 
+import styles from '../styles/book.module.css';
+
 const Book = (props) => {
   const { book } = props;
   const { id, title, author } = book;
@@ -9,25 +11,36 @@ const Book = (props) => {
   const dispatch = useDispatch();
 
   const deleteBook = (event) => {
-    const { id } = event.target.parentElement;
+    const { id } = event.target.parentElement.parentElement.parentElement;
     removeBookAPI(id, dispatch);
   };
 
   return (
-    <li id={id}>
-      <h3>
-        {title}
-        {' '}
-        {author}
-      </h3>
-      <button
-        type="button"
-        onClick={(event) => {
-          deleteBook(event);
-        }}
-      >
-        remove
-      </button>
+    <li className={styles.bookPanel} id={id}>
+      <div>
+        <h3 className="category">Action</h3>
+        <h2 className="title">{title}</h2>
+        <h6 className="author">{author}</h6>
+        <div className="buttonsWrapper">
+          <button className="button" type="button">
+            Comments
+          </button>
+          <div className="separator" />
+          <button
+            className="button"
+            type="button"
+            onClick={(event) => {
+              deleteBook(event);
+            }}
+          >
+            remove
+          </button>
+          <div className="separator" />
+          <button className="button" type="button">
+            Edit
+          </button>
+        </div>
+      </div>
     </li>
   );
 };
--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import BooksList from './BooksList';
 import InputBook from './InputBook';
 
 const BooksPage = () => (
-  <div>
+  <>
     <BooksList />
     <InputBook />
-  </div>
+  </>
 );
 
 export default BooksPage;
