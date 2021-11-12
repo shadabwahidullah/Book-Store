59aba823594e67d65e6b8177ec9f790b3e3ce90c
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-13 02:16:50 +0430

    Add profile icon

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 import BooksPage from './components/BooksPage';
 import Categories from './components/Categories';
 
@@ -9,15 +10,22 @@ const App = () => (
   <div className="panel-bg">
     <Router>
       <nav className="navbar">
-        <h1>Awesome Books CMS</h1>
-        <ul className="nav-list">
-          <li>
-            <Link className="nav-item" to="/">Books</Link>
-          </li>
-          <li>
-            <Link className="nav-item" to="/categories">Categories</Link>
-          </li>
-        </ul>
+        <div className="nav-brand">
+          <h1>Awesome Books CMS</h1>
+          <ul className="nav-list">
+            <li>
+              <Link className="nav-item" to="/">
+                Books
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-item" to="/categories">
+                Categories
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <FaUser className="user" />
       </nav>
       <Switch>
         <Route exact path="/categories">
